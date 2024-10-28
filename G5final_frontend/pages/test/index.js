1bf587d0aec94b7f31557947e34e96068ad94c9d
Yen <seesaw226357@gmail.com>
2024-10-28 11:50:27 +0800

    test路由套用會員sidebar改變寫法

--- G5final_frontend/pages/test/index.js
+++ G5final_frontend/pages/test/index.js
@@ -1,19 +1,15 @@
-// pages/special-page.js
 import React from 'react';
+// 定義 getLayout 方法來指定使用 AdminLayout
 import AdminLayout from '@/components/layout/admin-layout';
+memberpage.getLayout = function getLayout(page) {
+  return <AdminLayout>{page}</AdminLayout>;
+};
 
-const memberpage = () => {
+export default function memberpage() {
   return (
     <div>
       <h1>會員佈局頁</h1>
       <p>這個頁面使用 AdminLayout佈局。</p>
     </div>
   );
-};
-
-// 定義 getLayout 方法來指定使用 AdminLayout
-memberpage.getLayout = function getLayout(page) {
-  return <AdminLayout>{page}</AdminLayout>;
-};
-
-export default memberpage;
+}
