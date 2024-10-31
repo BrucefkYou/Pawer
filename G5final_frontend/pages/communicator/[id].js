e54206e0cbeaa9020a1d737711be2c522126a661
Yen <seesaw226357@gmail.com>
2024-10-31 19:28:11 +0800

    細節頁讀取資料庫完成

--- G5final_frontend/pages/communicator/[id].js
+++ G5final_frontend/pages/communicator/[id].js
@@ -3,6 +3,11 @@ import Breadcrumbs from '@/components/breadcrumbs/breadcrumbs';
 import PetDetail from '@/components/pet/pet-detail';
 
 export default function Id(props) {
+  const router = useRouter();
+
+  const handleBack = () => {
+    router.push('/communicator'); // 替換為目標頁面的路徑
+  };
   return (
     <>
       <div className="PT-detail">
