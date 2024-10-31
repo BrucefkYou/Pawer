8a0b9203d14fa678137198a1413cb9ca4e7d6a23
Bruce <paydayucky@gmail.com>
2024-10-31 21:58:12 +0800

    移除購物車page內的products範例的註解

--- G5final_frontend/pages/cart/products.js
+++ G5final_frontend/pages/cart/products.js
@@ -5,33 +5,6 @@ import { useCart } from '@/hooks/use-cart/use-cart-state';
 export default function Products(props) {
   const [products, setProducts] = useState([]);
   // 將商品加入購物車
-  // const addProductToCart = (product) => {
-  //   // 1. 讀取現有的購物車資料，若不存在則初始化為空陣列
-  //   const existingCart = JSON.parse(localStorage.getItem('cart')) || [];
-  //   // 2. 檢查商品是否已存在於購物車中（根據商品 ID）
-  //   // 如果some()回傳true，表示陣列中有任一元素符合條件
-  //   const isProductInCart = existingCart.some((item) => {
-  //     console.log('itemID: ' + item.id, 'productID:' + product.ID);
-  //     return item.id === product.id;
-  //   });
-
-  //   if (isProductInCart) {
-  //     // 如果商品已存在，您可以選擇增加數量或提示用戶
-  //     alert('此商品已在購物車中！');
-  //   } else {
-  //     // 如果商品不存在，將其加入購物車
-  //     existingCart.push({
-  //       id: product.ID,
-  //       name: product.Name,
-  //       price: product.OriginPrice,
-  //       quantity: 1, // 初始數量
-  //       // 其他需要的屬性
-  //     });
-
-  //     // 3. 將更新後的購物車資料儲存回 localStorage
-  //     localStorage.setItem('cart', JSON.stringify(existingCart));
-  //   }
-  // };
   const {
     cart,
     items,
