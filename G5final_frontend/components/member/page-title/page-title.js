d43bd775c55d0b80c500fd97490c8303739cd4c6
Edward <edward940715@gmail.com>
2024-11-02 19:14:41 +0800

    調整會員標題元件

--- G5final_frontend/components/member/page-title/page-title.js
+++ G5final_frontend/components/member/page-title/page-title.js
@@ -1,10 +1,9 @@
-import React, { useState, useEffect } from 'react';
 import styles from './page-title.module.scss';
 
 export default function PageTitle({ title, subTitle }) {
   return (
     <>
-      <h5 className="title">
+      <h5 className={`${styles['title']}`}>
         {title} <span className="text-warning">{subTitle}</span>
         <div className={`${styles['underline']}`}>
           <div className={`${styles['underline-part1']}`}></div>
