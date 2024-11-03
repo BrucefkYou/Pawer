983d4dc643df519feac01f204f630e4b5b5faafe
Maggie <humaggie79@gmail.com>
2024-11-04 00:59:33 +0800

    CK高度，placeholder調整

--- G5Final_backend/routes/join-in.js
+++ G5Final_backend/routes/join-in.js
@@ -36,9 +36,32 @@ LEFT JOIN
     Image ON Image.JoininID = Joinin.ID
 WHERE 
     Joinin.Valid = 1
+      SELECT 
+    Joinin.*,
+    Image.ImageID,
+    Image.ImageName,
+    Image.ImageUrl,
+    (SELECT COUNT(*) FROM MemberFavoriteMapping WHERE MemberFavoriteMapping.JoininID = Joinin.ID) AS joinFavCount,
+    (SELECT COUNT(*) 
+     FROM Joined 
+     WHERE Joined.JoininID = Joinin.ID AND Status = 1) AS SignCount,
+    CASE
+        WHEN (SELECT COUNT(*) FROM Joined WHERE Joined.JoininID = Joinin.ID AND Status = 1) + 5 >= Joinin.ParticipantLimit THEN '即將成團'
+        WHEN (SELECT COUNT(*) FROM Joined WHERE Joined.JoininID = Joinin.ID AND Status = 1) >= Joinin.ParticipantLimit THEN '已額滿'
+        WHEN CURRENT_TIMESTAMP > Joinin.SignEndTime THEN '截止報名'
+        WHEN CURRENT_TIMESTAMP BETWEEN Joinin.CreateDate AND Joinin.SignEndTime THEN '報名中'
+        ELSE '未開放'
+    END AS newStatus
+FROM 
+    Joinin
+LEFT JOIN 
+    Image ON Image.JoininID = Joinin.ID
+WHERE 
+    Joinin.Valid = 1
     `)
 
     res.json(rows)
+    console.log(rows)
     console.log(rows)
   } catch (err) {
     console.error('查詢錯誤：', err)
@@ -70,6 +93,35 @@ router.get('/:id', async function (req, res, next) {
 FROM 
     Joinin
 LEFT JOIN 
+    Image ON Image.JoininID = Joinin.ID
+     WHERE ID = ?`,
+      [req.params.id]
+    )
+    // 檢查是否有找到資料
+    if (rows.length === 0) {
+      return res.status(404).json({ message: '找不到指定的資料' })
+    }
+    res.json(rows[0]) // 因為只會有一筆資料，所以直接返回第一個元素
+  } catch (err) {
+    console.error('查詢錯誤：', err)
+    res.status(500).send(err)
+  }
+})
+
+router.put('/:id', async function (req, res, next) {
+  try {
+    // 使用 WHERE 子句來篩選指定 id 的資料
+    const [rows] = await db2.query(
+      `SELECT 
+    Joinin.*,
+    Image.ImageID,
+    Image.ImageName,
+    Image.ImageUrl,
+    (SELECT COUNT(*) FROM MemberFavoriteMapping WHERE MemberFavoriteMapping.JoininID = Joinin.ID) AS joinFavCount,
+    (SELECT COUNT(*) 
+    FROM Joined 
+    WHERE Joined.JoininID = Joinin.ID AND Status = 1) AS SignCount,
+    LEFT JOIN 
     Image ON Image.JoininID = Joinin.ID
      WHERE ID = ?`,
       [req.params.id]
