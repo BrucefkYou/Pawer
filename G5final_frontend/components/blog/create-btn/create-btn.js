3c352bf7a5ef4f3297312db8c3f15c255a81aaac
Alice <morning.chung91@gmail.com>
2024-11-04 11:44:25 +0800

    新增部落格頁面、搜尋、router

--- G5final_frontend/components/blog/create-btn/create-btn.js
+++ G5final_frontend/components/blog/create-btn/create-btn.js
@@ -1,20 +1,25 @@
-import { BsPencilFill } from "react-icons/bs";
-import { useState, useEffect } from 'react';
-import styles from './create-btn.module.scss';
+import { BsPencilFill } from 'react-icons/bs'
+import { useState, useEffect } from 'react'
+import styles from './create-btn.module.scss'
+import Link from 'next/link'
 
 export default function CreateBtn({ btnName }) {
-  const [isMobile, setIsMobile] = useState(false);
+  const [isMobile, setIsMobile] = useState(false)
 
   useEffect(() => {
-    const resize = () => setIsMobile(window.innerWidth < 768);
-    resize(); 
-    window.addEventListener('resize', resize);
-    return () => window.removeEventListener('resize', resize);
-  }, []);
+    const resize = () => setIsMobile(window.innerWidth < 768)
+    resize()
+    window.addEventListener('resize', resize)
+    return () => window.removeEventListener('resize', resize)
+  }, [])
 
   return (
-      <button className={`btn btn-warning ${styles['create-btn']}`}>
-        {isMobile ? <BsPencilFill size={20} color="white" /> : btnName}
-      </button>
-  );
+    <Link
+      href={`http://localhost:3000/blog/create`}
+      className={`btn btn-warning ${styles['create-btn']}`}
+      type="button"
+    >
+      {isMobile ? <BsPencilFill size={20} color="white" /> : btnName}
+    </Link>
+  )
 }
