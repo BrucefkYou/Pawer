8baf16ce5b006d9f85181c8835a5b14fd55fbea2
Bruce <paydayucky@gmail.com>
2024-11-04 10:45:14 +0800

    修正alt錯誤&&更新漢堡圖片檔案成png

--- G5final_frontend/components/layout/default-layout/navbar.js
+++ G5final_frontend/components/layout/default-layout/navbar.js
@@ -74,20 +74,7 @@ export default function Navbar() {
                   aria-label="切換下拉選單"
                 >
                   {/* 下拉選單 SVG 圖示 */}
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    width={25}
-                    height={25}
-                    viewBox="0 0 25 25"
-                    fill="none"
-                  >
-                    <path
-                      fillRule="evenodd"
-                      clipRule="evenodd"
-                      d="M2 21C2 20.7348 2.10536 20.4804 2.29289 20.2929C2.48043 20.1054 2.73478 20 3 20H23C23.2652 20 23.5196 20.1054 23.7071 20.2929C23.8946 20.4804 24 20.7348 24 21C24 21.2652 23.8946 21.5196 23.7071 21.7071C23.5196 21.8946 23.2652 22 23 22H3C2.73478 22 2.48043 21.8946 2.29289 21.7071C2.10536 21.5196 2 21.2652 2 21ZM2 13C2 12.7348 2.10536 12.4804 2.29289 12.2929C2.48043 12.1054 2.73478 12 3 12H23C23.2652 12 23.5196 12.1054 23.7071 12.2929C23.8946 12.4804 24 12.7348 24 13C24 13.2652 23.8946 13.5196 23.7071 13.7071C23.5196 13.8946 23.2652 14 23 14H3C2.73478 14 2.48043 13.8946 2.29289 13.7071C2.10536 13.5196 2 13.2652 2 13ZM2 5C2 4.73478 2.10536 4.48043 2.29289 4.29289C2.48043 4.10536 2.73478 4 3 4H23C23.2652 4 23.5196 4.10536 23.7071 4.29289C23.8946 4.48043 24 4.73478 24 5C24 5.26522 23.8946 5.51957 23.7071 5.70711C23.5196 5.89464 23.2652 6 23 6H3C2.73478 6 2.48043 5.89464 2.29289 5.70711C2.10536 5.51957 2 5.26522 2 5Z"
-                      fill="white"
-                    />
-                  </svg>
+                  <Image width={21} height={17} alt='hamburger' src={"/hamburger.png"}/>
                 </button>
                 <div
                   className={`customDropdownMenu ${isDropdownOpen ? 'showMenu' : ''
@@ -189,10 +176,10 @@ export default function Navbar() {
             {auth.isAuth ? (
               member.avatar ? ( 
                 <button className="navbar-member" onClick={islogin}>
-                  <Image width={24} height={24} objectFit='cover' className='navbar-login-img' src={`/member/member-avatar/${member.avatar}`} />
+                  <Image width={24} height={24} className='navbar-login-img' src={`/member/member-avatar/${member.avatar}`} alt='使用者頭像' />
                 </button>
               ) : (<button className="navbar-member" onClick={islogin}>
-                <Image width={24} height={24} objectFit='cover' src={`/member/member-profile.png`} />
+                <Image width={24} height={24} src={`/member/member-profile.png`} alt='預設使用者頭像' />
               </button>)
             ) : (<button className="navbar-member" onClick={islogin}>
               <BsPerson className="text-secondary" />
