455f6f7e59e02d14631e5f4446f9493a05e3b2ca
Yen <seesaw226357@gmail.com>
2024-11-05 14:29:25 +0800

    範例修正

--- G5final_frontend/pages/sample/index.js
+++ G5final_frontend/pages/sample/index.js
@@ -7,9 +7,9 @@ export default function Index(props) {
           
           <div className='container d-flex flex-column'>
               <h1> 範例區</h1>
-              <Link href={'/sample-memberNav'}>會員頁籤範例</Link>
-              <Link href={'/sample-Perpage'}>頁碼/每頁幾筆/排序範例</Link>
-              <Link href={'/sample-searchbar'}>搜尋框範例</Link>
+              <Link href={'/sample/sample-memberNav'}>會員頁籤範例</Link>
+              <Link href={'/sample/sample-Perpage'}>頁碼/每頁幾筆/排序範例</Link>
+              <Link href={'/sample/sample-searchbar'}>搜尋框範例</Link>
           </div>
           
     </>
