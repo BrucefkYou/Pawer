eac308d4d3bcce8dd2480af82429721007d52ce1
Yen <seesaw226357@gmail.com>
2024-11-06 19:34:02 +0800

    router新增

--- G5Final_backend/routes/pet.js
+++ G5Final_backend/routes/pet.js
@@ -1,5 +1,6 @@
 import express from 'express'
 import db2 from '../configs/mysql.js'
+import authenticate from '##/middlewares/authenticate.js'
 const router = express.Router()
 
 /* GET home page. */
@@ -15,6 +16,20 @@ router.get('/', async function (req, res, next) {
 router.get('/comreserve', async function (req, res, next) {
   try {
     const [rows] = await db2.query('SELECT * FROM `PetCommunicatorReserve`') // 確認資料表名稱是否正確
+    res.json(rows)
+  } catch (err) {
+    console.error('查詢錯誤：', err)
+    res.status(500).send(err)
+  }
+})
+
+router.post('/p', async function (req, res, next) {
+  const { ID } = req.body
+  try {
+    const [rows] = await db2.query(
+      'SELECT * FROM `PetCommunicator` WHERE ID = ?',
+      [ID]
+    ) // 確認資料表名稱是否正確
     res.json(rows)
   } catch (err) {
     console.error('查詢錯誤：', err)
