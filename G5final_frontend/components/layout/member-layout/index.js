738fd0d8d84020f8dbfe77f23a4fe3c161b5c89b
Yen <seesaw226357@gmail.com>
2024-11-08 18:47:45 +0800

    memberlayout修正

--- G5final_frontend/components/layout/member-layout/index.js
+++ G5final_frontend/components/layout/member-layout/index.js
@@ -15,7 +15,7 @@ export default function MemberLayout({ children }) {
               {/* <aside className="col-3 d-none d-lg-block"> */}
               <MbSideBar />
             </aside>
-            <article className="col-md-9 d-flex justify-content-center align-items-center">
+            <article className="col-md-9">
               {children}
             </article>
           </div>
