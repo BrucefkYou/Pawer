e41a6c4a19ea08843c77108fee28b1635bd7917a
Weicinnn <z0091220@gmail.com>
2024-11-09 23:59:55 +0800

    商品細節頁輪播圖無法顯示修正,加入購物車新增提示,修改細節頁RWD輪播畫面,image.json新增,models新增favorite用於存商品收藏

--- G5final_frontend/components/product/productList.js
+++ G5final_frontend/components/product/productList.js
@@ -14,40 +14,40 @@ export default function ProductList({ pd, setUrl }) {
     <>
       <div className="col-4 card-layout no-underline">
         <div className="card shadow card-size">
-          <Image
-            className="card-img-top card-img-topme"
-            alt="商品列表圖"
-            src={`/product/sqlimg/${pd.Img}`}
-            width={640}
-            height={640}
-            priority
-          />
-          <div className="card-body">
-            <Link
-              key={`${pd.ID}-${pd.Name}`}
-              href={`/product/${pd.ID}`}
-              className="pd-card-text pd-card-textD no-underline"
-            >
+          <Link
+            key={`${pd.ID}-${pd.Name}`}
+            href={`/product/${pd.ID}`}
+            className="no-underline"
+          >
+            <Image
+              className="card-img-top card-img-topme"
+              alt="商品列表圖"
+              src={`/product/sqlimg/${pd.Img}`}
+              width={640}
+              height={640}
+              priority
+            />
+            <div className="card-body">
               <div className="pd-card-text no-underline">{pd.Name}</div>
               <div className="pd-card-textD no-underline">{pd.Name}</div>
-            </Link>
-
-            <div className="d-flex justify-content-between mt-3">
-              <div className="card-text">
-                <small className="be-nt">{'NT$' + pd.OriginPrice}</small>
-              </div>
             </div>
-            <div className="d-flex justify-content-between">
-              <div className="new-nt">{'NT$' + pd.SalePrice}</div>
-              {/* 收藏icon */}
-              <div className={`${style['pdsvg-favorite']}`}>
-                <FavoriteIcon
-                  setUrl={setUrl}
-                  IconFilled={BsBookmarkFill}
-                  IconOutline={BsBookmark}
-                  pd={pd.ID}
-                />
-              </div>
+          </Link>
+
+          <div className="d-flex justify-content-between">
+            <div className="card-text">
+              <small className="be-nt ms-4">{'NT$' + pd.OriginPrice}</small>
+            </div>
+          </div>
+          <div className="d-flex justify-content-between">
+            <div className="new-nt ms-4">{'NT$' + pd.SalePrice}</div>
+            {/* 收藏icon */}
+            <div className={`${style['pdsvg-favorite']} me-2`}>
+              <FavoriteIcon
+                setUrl={setUrl}
+                IconFilled={BsBookmarkFill}
+                IconOutline={BsBookmark}
+                pd={pd.ID}
+              />
             </div>
           </div>
         </div>
