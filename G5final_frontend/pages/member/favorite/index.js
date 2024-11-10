8d95fd937377ed5812da1af7b5e76389759085fa
Weicinnn <z0091220@gmail.com>
2024-11-10 17:42:00 +0800

    會員商品收藏頁樣式更改,把分頁及收藏數量加入

--- G5final_frontend/pages/member/favorite/index.js
+++ G5final_frontend/pages/member/favorite/index.js
@@ -1,34 +1,49 @@
 import React, { useState, useEffect } from 'react';
+import { PageNav } from '@/components/PageNav';
 import { usePagination } from '@/hooks/usePagination';
 import MemberLayout from '@/components/layout/member-layout';
 import PageTitle from '@/components/member/page-title/page-title';
 import ProductList from '@/components/product/productList';
 import Link from 'next/link';
 
+import MemberNav from '@/components/memberNav';
+
 Index.getLayout = function getLayout(page) {
   return <MemberLayout>{page}</MemberLayout>;
 };
 
 export default function Index() {
-  const [favorites, setFavorites] = useState([]);
   const [url, setUrl] = useState(
     'http://localhost:3005/api/product/member/favorite'
   );
 
   // setUrl 第一層在父層 帶下去商品卡片頁第二層子層
 
-  const { nowPageItems } = usePagination({
+  const {
+    newdata,
+    needFilter,
+    nowPageItems,
+    nowPage,
+    totalPage,
+    next,
+    prev,
+    chooseFilter,
+  } = usePagination({
     url: url,
-    needFilter: [],
+    needFilter: [{ id: 1, label: '已收藏' }],
   });
-
   return (
-    <div>
-      <div className="card">
+    <div className="productList">
+      <div className="card-favorite d-flex justify-content-between">
         <PageTitle title={'收藏商品'} subTitle={'Favorite'} />
+        <MemberNav
+          newdata={newdata}
+          chooseFilter={chooseFilter}
+          needFilter={needFilter}
+        />
       </div>
 
-      <div className="productList">
+      <div>
         {nowPageItems.length === 0 ? (
           <>
             <p className="mt-2">您沒有收藏的商品</p>
@@ -37,12 +52,25 @@ export default function Index() {
             </Link>
           </>
         ) : (
-          <div className="row ms-4 d-flex justify-content-start">
+          <div className="row ms-4 d-flex justify-content-start card-favorite-list">
             {nowPageItems.map((pd) => (
               <ProductList key={pd.id} pd={pd} setUrl={setUrl} />
             ))}
           </div>
         )}
+        {/* 頁碼 */}
+        <div className="d-flex justify-content-center align-items-center mt-3">
+          {nowPageItems.length === 0 ? (
+            <div></div>
+          ) : (
+            <PageNav
+              nowPage={nowPage}
+              totalPage={totalPage}
+              next={next}
+              prev={prev}
+            />
+          )}
+        </div>
       </div>
     </div>
   );
