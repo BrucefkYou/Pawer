6e4e4c91b0028db2e9e63cc7890353705b5d3032
Edward <edward940715@gmail.com>
2024-11-11 17:42:56 +0800

    修正member model表移除自動新增時間戳欄位

--- G5Final_backend/models/Member.js
+++ G5Final_backend/models/Member.js
@@ -58,11 +58,11 @@ export default async function (sequelize) {
     },
     {
       tableName: 'Member', //直接提供資料表名稱
-      timestamps: true, // 使用時間戳
+      timestamps: false, // 使用時間戳
       paranoid: false, // 軟性刪除
       underscored: false, // 所有自動建立欄位，使用snake_case命名
-      createdAt: 'created_at', // 建立的時間戳
-      updatedAt: 'updated_at', // 更新的時間戳
+      // createdAt: 'created_at', // 建立的時間戳
+      // updatedAt: 'updated_at', // 更新的時間戳
     }
   )
 }
