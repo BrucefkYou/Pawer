ee92cd9b063fa46d635105f6903b59c2f9883158
Edward <edward940715@gmail.com>
2024-11-11 10:20:19 +0800

    googlelogin尚未完成

--- G5final_frontend/services/member.js
+++ G5final_frontend/services/member.js
@@ -1,8 +1,6 @@
 import axiosInstance from './axios-instance';
 
-/**
- * 修改會員一般資料用(排除password, username, email)
- */
+//  修改會員一般資料用(排除password, username, email)
 export const updateProfile = async (memberId = 0, user = {}) => {
   return await axiosInstance.put(`/member/profile/${memberId}`, user);
 };
@@ -21,4 +19,16 @@ export const getOrder = async (memberId = 0, orderId = 0) => {
 
 export const getCouponsByUser = async (memberId = 0) => {
   return await axiosInstance.get(`/member/${memberId}/coupons`);
+};
+
+// Google Login(Firebase)登入用，providerData為登入後得到的資料
+export const googleLogin = async (providerData = {}) => {
+  return await axiosInstance.post('/google-login', providerData);
+};
+
+// 解析accessToken用的函式
+export const parseJwt = (token) => {
+  const base64Payload = token.split('.')[1];
+  const payload = Buffer.from(base64Payload, 'base64');
+  return JSON.parse(payload.toString());
 };
