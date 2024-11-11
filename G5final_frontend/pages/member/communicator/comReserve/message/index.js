82a2fb9ecda00432e7d201e62f859bd535b927f5
Yen <seesaw226357@gmail.com>
2024-11-11 13:27:40 +0800

    提示文字修正

--- G5final_frontend/pages/member/communicator/comReserve/message/index.js
+++ G5final_frontend/pages/member/communicator/comReserve/message/index.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import MemberLayout from '@/components/layout/member-layout';
 import Link from 'next/link';
-import { BsCheckCircle } from "react-icons/bs";
+import { BsCheckCircle } from 'react-icons/bs';
 Message.getLayout = function getLayout(page) {
   return <MemberLayout>{page}</MemberLayout>;
 };
@@ -10,12 +10,16 @@ export default function Message(props) {
     <>
       <div className="message-card">
         <div className="pic mb-4">
-          <BsCheckCircle style={{fontSize:'80px', color:'green'}}/>
+          <BsCheckCircle style={{ fontSize: '80px', color: 'green' }} />
         </div>
         <h3>取消預約成功！</h3>
-        <p>期待下次光臨</p>
+        <hr />
+        <p></p>
+
         <p className="text1">其餘問題可洽客服</p>
-        <Link href={'/member/communicator/comReserve'} className="message-btn">返回預約列表</Link>
+        <Link href={'/member/communicator/comReserve'} className="message-btn">
+          返回預約列表
+        </Link>
       </div>
     </>
   );
