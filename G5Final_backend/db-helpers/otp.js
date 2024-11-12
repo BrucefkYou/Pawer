c97ae854311d8fa00ae8e8470c913ed452eec1fe
Edward <edward940715@gmail.com>
2024-11-12 17:11:55 +0800

    新增忘記密碼寄信otp驗證

--- G5Final_backend/db-helpers/otp.js
+++ G5Final_backend/db-helpers/otp.js
@@ -1,15 +1,9 @@
 // 資料庫查詢處理函式
 import { generateToken } from '#configs/otp.js'
-
-// 資料庫使用
-import { QueryTypes } from 'sequelize'
-import sequelize from '#configs/db.js'
-const { User, Otp } = sequelize.models
-
-// 判斷token是否到期, true代表到期
-// const hasExpired = (expTimestamp) => {
-//   return Date.now() > expTimestamp
-// }
+// 資料庫使用，使用原本的mysql2 + sql
+import db from '##/configs/mysql.js'
+// 加密密碼字串用
+import { generateHash } from '#db-helpers/password-hash.js'
 
 // 判斷是否可以重設token, true代表可以重設
 const shouldReset = (expTimestamp, exp, limit = 60) => {
@@ -19,26 +13,28 @@ const shouldReset = (expTimestamp, exp, limit = 60) => {
 
 // exp = 是 30 分到期,  limit = 60 是 60秒內不產生新的token
 const createOtp = async (email, exp = 30, limit = 60) => {
-  // 方式二: 使用模型查詢
-  // 檢查使用者email是否存在
-  const user = await User.findOne({
-    where: {
-      email,
-    },
-    raw: true, // 只需要資料表中資料
-  })
+  console.log(`email`, email)
 
-  if (!user) {
+  // 檢查使用者email是否存在
+  const [userResult] = await db.execute(
+    'SELECT * FROM Member WHERE eMail = ?',
+    [email]
+  )
+
+  if (userResult.length === 0) {
     console.log('ERROR - 使用者帳號不存在'.bgRed)
     return {}
   }
+  const user = userResult[0]
+  console.log('user:', user)
+
   // 檢查otp是否已經存在
-  const foundOtp = await Otp.findOne({
-    where: {
-      email,
-    },
-    raw: true, // 只需要資料表中資料
-  })
+  const [foundOtpResult] = await db.execute(
+    'SELECT * FROM otp WHERE email = ?',
+    [email]
+  )
+  const foundOtp = foundOtpResult[0]
+  console.log('foundOtp:', foundOtp)
 
   // 找到記錄，因為在60s(秒)內限制，所以"不能"產生新的otp token
   if (foundOtp && !shouldReset(foundOtp.exp_timestamp, exp, limit)) {
@@ -55,84 +51,87 @@ const createOtp = async (email, exp = 30, limit = 60) => {
     const exp_timestamp = Date.now() + exp * 60 * 1000
 
     // 修改Otp
-    await Otp.update(
-      { token, exp_timestamp },
-      {
-        where: {
-          email,
-        },
-      }
+    const [updateOtpResults] = await db.execute(
+      'UPDATE otp SET token = ?, exp_timestamp = ? WHERE email = ?',
+      [token, exp_timestamp, email]
     )
 
-    return {
-      ...foundOtp,
-      exp_timestamp,
-      token,
+    if (!updateOtpResults) {
+      console.log('修改Otp失敗'.bgRed)
+      return {}
+    } else {
+      return {
+        ...foundOtp,
+        exp_timestamp,
+        token,
+      }
     }
   }
 
   // 以下為"沒找到otp記錄"
   // 以使用者輸入的Email作為secret產生otp token
   const token = generateToken(email)
+  console.log('token:', token)
 
   // 到期時間 預設 exp = 30 分鐘到期
   const exp_timestamp = Date.now() + exp * 60 * 1000
 
   // 建立otp物件
   const newOtp = {
-    user_id: user.id,
+    user_id: user.ID,
     email,
     token,
     exp_timestamp,
   }
+  console.log('newOtp:', newOtp)
 
   // 建立新記錄
-  const otp = await Otp.create(newOtp)
-  // console.log(otp.dataValues)
+  const [createOtpResults] = await db.execute(
+    'INSERT INTO otp (user_id, email,token,exp_timestamp) VALUE (?,?,?,?)',
+    [user.ID, email, token, exp_timestamp]
+  )
 
-  return otp.dataValues
+  if (!createOtpResults) {
+    console.log('新增Otp失敗'.bgRed)
+    return {}
+  } else {
+    return newOtp
+  }
 }
 
 // 更新密碼
 const updatePassword = async (email, token, password) => {
   // 檢查otp是否已經存在
-  const foundOtp = await Otp.findOne({
-    where: {
-      email,
-      token,
-    },
-    raw: true, // 只需要資料表中資料
-  })
-
+  const [otpResult] = await db.execute(
+    `SELECT * FROM Otp WHERE email = ? AND token = ? LIMIT 1`,
+    [email, token]
+  )
   // 沒找到回傳false
-  if (!foundOtp) {
+  if (otpResult.length === 0) {
     console.log('ERROR - OTP Token資料不存在'.bgRed)
     return false
   }
+  const foundOtp = otpResult[0]
 
-  // 計算目前時間比對是否超過，到期的timestamp
+  // 檢查 OTP 是否過期
   if (Date.now() > foundOtp.exp_timestamp) {
     console.log('ERROR - OTP Token已到期'.bgRed)
     return false
   }
 
+  // 密碼加密
+  const hashpassword = await generateHash(password)
+
   // 修改密碼
-  await User.update(
-    { password },
-    {
-      where: {
-        id: foundOtp.user_id,
-      },
-      individualHooks: true, // 密碼進資料表前要加密 trigger the beforeUpdate hook
-    }
+  await db.execute(
+    `UPDATE Member
+       SET Password = ?
+       WHERE ID = ?`,
+    [hashpassword, foundOtp.user_id]
   )
 
-  // 移除otp記錄
-  await Otp.destroy({
-    where: {
-      id: foundOtp.id,
-    },
-  })
+  // 移除 OTP 記錄
+  await db.execute(`DELETE FROM otp WHERE id = ?`, [foundOtp.id])
 
   return true
 }
