c4b9ecd1f757701929fd20b978f1afddd49c3a94
Edward <edward940715@gmail.com>
2024-11-12 09:31:16 +0800

    調整頭像顯示順序

--- G5final_frontend/hooks/use-auth.js
+++ G5final_frontend/hooks/use-auth.js
@@ -18,6 +18,7 @@ export function AuthProvider({ children }) {
     nickname: '',
     avatar: '',
     google_uid: '',
+    google_avatar: '',
   };
 
   //定義登入狀態與會員資料(可從此取得會員資料id, name, email, nickname, avatar，若需要更多就撈出id自行去撈db)
@@ -59,6 +60,7 @@ export function AuthProvider({ children }) {
             nickname: res.data.memberData.Nickname ?? '',
             avatar: res.data.memberData.Avatar ?? '',
             google_uid: res.data.memberData.google_uid ?? '',
+            google_avatar: res.data.memberData.google_avatar ?? '',
           },
         });
         // 導向到會員中心
@@ -148,6 +150,7 @@ export function AuthProvider({ children }) {
             nickname: res.data.memberData.Nickname ?? '',
             avatar: res.data.memberData.Avatar ?? '',
             google_uid: res.data.memberData.google_uid ?? '',
+            google_avatar: res.data.memberData.google_avatar ?? '',
           },
         };
         setAuth(nextAuth);
