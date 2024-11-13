8691b9c71047eb904585ecc904eaf397b5b11550
Yen <seesaw226357@gmail.com>
2024-11-13 18:11:22 +0800

    師資預約列表頁修正

--- G5final_frontend/components/pet/comReserve/ComListButton.js
+++ G5final_frontend/components/pet/comReserve/ComListButton.js
@@ -1,9 +1,8 @@
 import React, { useState, useEffect } from 'react'
 import Router, { useRouter } from 'next/router';
 import Link from 'next/link';
-export default function ComListButton({ v }) {
+export default function ComListButton({ v, setMessage }) {
     const router = useRouter()
-
     async function cancelReserve(event) {
         try {
             await fetch('http://localhost:3005/api/pet/cancelReserve', {
@@ -13,10 +12,11 @@ export default function ComListButton({ v }) {
                 },
                 body: JSON.stringify({ ID: v.ID })
             })
-            router.push('/member/communicator/comReserve/message')
+            setMessage('ok')
         }
         catch (err) {
             console.log(err);
+            setMessage('no')
         }
     }
     return (
@@ -29,9 +29,9 @@ export default function ComListButton({ v }) {
                     <Link href={`/websocket?call=${v.MemberID}`} className={`btnn btnn-2 m-0 ${v.Status == 1 ? 'PT-sp-block' : 'PT-sp-none'}`}>
                         聯繫毛孩爸媽
                     </Link>
-                    <button className={`btnn btnn-3 m-0 ${v.Status == 1 ? 'PT-sp-none' : 'PT-sp-block'} PT-sp-none-rwd`}>
+                    <Link href={`/websocket?call=${v.MemberID}`} className={`btnn btnn-3 m-0 ${v.Status == 1 ? 'PT-sp-none' : 'PT-sp-block'} PT-sp-none-rwd`}>
                         聯繫毛孩爸媽
-                    </button>
+                    </Link>
                 </div>
             </div>
         </>
