214b5c40eb2efe3dfa43f2f096b87311dd01a43c
Yen <seesaw226357@gmail.com>
2024-11-16 20:16:23 +0800

    渲染順序異常debug

--- G5final_frontend/components/pet/memReserve/MemReserve.js
+++ G5final_frontend/components/pet/memReserve/MemReserve.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState, useEffect } from 'react';
 import { useAuth } from '@/hooks/use-auth';
 import { usePagination } from '@/hooks/usePagination';
 import PageTitle from '@/components/member/page-title/page-title';
@@ -8,73 +8,106 @@ import MemReserveList from './MemReserveList';
 import Link from 'next/link';
 import Message from '../message';
 export default function MemReserve(props) {
-    const { auth } = useAuth()
-    const id = auth.memberData.id
-    // 定義資料處理函數
-    const processData = (fetchedData) => {
-        return fetchedData.filter((item) => {
-            return item.MemberID == id
-        });
-    };
-    // 提示頁面狀態
-    const [message, setMessage] = useState('')
-    if (message == 'ok') {
-        return <Message status='ok' title='hi' content='' button='返回' url='/member/communicator/comReserve' />
-    } else if (message == 'warn') {
-        return <Message status='warn' title='hi' content='' button='返回' url='/member/communicator/comReserve' />
-    } else if (message == 'no') {
-        return <Message status='no' title='hi' content='' button='返回' url='/member/communicator/comReserve' />
-    }
-    const {
-        chooseFilter,
-        newdata,
-        nowPageItems,
-        needFilter,
-        nowPage,
-        totalPage,
-        next,
-        prev,
-    } = usePagination({
-        url: 'http://localhost:3005/api/pet/memreserve',
-        needFilter: [
-            { id: 1, label: '預約中', filterRule: '1', filterName: 'Status' },
-            { id: 2, label: '歷史', filterRule: '0', filterName: 'Status' },
-        ],
-        needSort: [{ way: 'desc-ID', name: '' }],
-        processData
+  const { auth } = useAuth();
+  // 定義資料處理函數
+  const processData = (fetchedData) => {
+    return fetchedData.filter((item) => {
+      return item.MemberID == id;
     });
+  };
+  const id = auth.memberData.id;
+  const {
+    chooseFilter,
+    newdata,
+    nowPageItems,
+    needFilter,
+    nowPage,
+    totalPage,
+    next,
+    prev,
+  } = usePagination({
+    url: 'http://localhost:3005/api/pet/memreserve',
+    needFilter: [
+      { id: 1, label: '預約中', filterRule: '1', filterName: 'Status' },
+      { id: 2, label: '歷史', filterRule: '0', filterName: 'Status' },
+    ],
+    needSort: [{ way: 'desc-ID', name: '' }],
+    processData,
+  });
 
+  // 提示頁面狀態
+  const [message, setMessage] = useState('');
+  if (message == 'ok') {
     return (
+      <Message
+        status="ok"
+        title="取消預約成功"
+        content="期待下次光臨"
+        button="返回"
+        url="/member/communicator/comReserve"
+      />
+    );
+  } else if (message == 'warn') {
+    return (
+      <Message
+        status="warn"
+        title="錯誤"
+        content=""
+        button="返回"
+        url="/member/communicator/comReserve"
+      />
+    );
+  } else if (message == 'no') {
+    return (
+      <Message
+        status="no"
+        title="執行失敗"
+        content=""
+        button="返回"
+        url="/member/communicator/comReserve"
+      />
+    );
+  }
+
+  return (
+    <>
+      <div className="d-flex justify-content-between">
+        {/* 標題 */}
+        <PageTitle title={'預約清單'} subTitle={'Reserve'} />
+        {/* 頁籤 */}
+        <MemberNav
+          newdata={newdata}
+          chooseFilter={chooseFilter}
+          needFilter={needFilter}
+        />
+      </div>
+      {/* 清單明細 */}
+      {nowPageItems.length < 1 ? (
+        <Link href={'/communicator'} className="btn btn-danger mt-3">
+          ＜ 目前無紀錄 點擊前往預約 ＞
+        </Link>
+      ) : (
         <>
-            <div className="d-flex justify-content-between">
-                {/* 標題 */}
-                <PageTitle title={'預約清單'} subTitle={'Reserve'} />
-                {/* 頁籤 */}
-                <MemberNav
-                    newdata={newdata}
-                    chooseFilter={chooseFilter}
-                    needFilter={needFilter}
-                />
-            </div>
-            {/* 清單明細 */}
-            {nowPageItems.length < 1 ?
-                (<Link href={'/communicator'} className='btn btn-danger mt-3'>＜ 目前無紀錄 點擊前往預約 ＞</Link>) :
-                <>
-                    <div className="row none title text-center mt-3 py-2">
-                        <div className="col-1">序號</div>
-                        <div className="col-2">溝通師</div>
-                        <div className="col-2 d-none d-lg-block">寵物名稱</div>
-                        <div className="col d-none d-lg-block">狀態</div>
-                        <div className="col">預約日期</div>
-                        <div className="col">預約時段</div>
-                        <div className="col-1" />
-                    </div>
-                    <MemReserveList nowPageItems={nowPageItems} setMessage={setMessage} />
-                    <div className='d-flex justify-content-center'>
-                        <PageNav nowPage={nowPage} totalPage={totalPage} next={next} prev={prev} />
-                    </div>
-                </>
-            }
+          <div className="row none title text-center mt-3 py-2">
+            <div className="col-1">序號</div>
+            <div className="col-2">溝通師</div>
+            <div className="col-2 d-none d-lg-block">寵物名稱</div>
+            <div className="col d-none d-lg-block">狀態</div>
+            <div className="col">預約日期</div>
+            <div className="col">預約時段</div>
+            <div className="col-1" />
+          </div>
+          <MemReserveList nowPageItems={nowPageItems} setMessage={setMessage} />
+          <div className="d-flex justify-content-center">
+            <PageNav
+              nowPage={nowPage}
+              totalPage={totalPage}
+              next={next}
+              prev={prev}
+            />
+          </div>
         </>
-    )
+      )}
+    </>
+  );
 }
