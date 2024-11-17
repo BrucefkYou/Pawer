40ebedff214ecccd52007b54cbeef4f1367ebc93
Weicinnn <z0091220@gmail.com>
2024-11-18 00:47:11 +0800

    更改商品列表頁篩選RWD

--- G5final_frontend/pages/product/tagdog/index.js
+++ G5final_frontend/pages/product/tagdog/index.js
@@ -14,12 +14,11 @@ import Breadcrumbs from '@/components/breadcrumbs/breadcrumbs';
 import ProductList from '@/components/product/list/productList';
 import Clean from '@/components/product/clean/clean';
 import CategoryOther from '@/components/product/category/other/categoryOther';
-import CategoryCat from '@/components/product/category/cat/categoryCat';
 import CategoryDog from '@/components/product/category/dog/categoryDog';
 import useCategory from '@/hooks/useCategory';
-import TagCat from '@/components/product/tag/tagCat/tagCat';
-import TagDog from '@/components/product/tag/tagDog/tagDog';
-import TagOther from '@/components/product/tag/tagOther/tagOther';
+import List from '@/components/product/tag/all/list';
+import Dog from '@/components/product/tag/dog/dog';
+import OtherDog from '@/components/product/tag/other/otherDog';
 
 export default function Tagdog(props) {
   const [url, setUrl] = useState('http://localhost:3005/api/product/tagdog');
@@ -96,9 +95,15 @@ export default function Tagdog(props) {
           </div>
           <div className="row rwd-select">
             <div className="ms-5 howmaney howmaney-rwd col mt-3 d-flex justify-content-between">
-              顯示第{nowPageFirstItems + 1}-
-              {Math.min(nowPageLastItems, filterData.length)} 筆 / 共{' '}
-              {filterData.length} 筆
+              {filterData.length === 0 ? (
+                <div></div>
+              ) : (
+                <p>
+                  顯示第{nowPageFirstItems + 1}-
+                  {Math.min(nowPageLastItems, filterData.length)} 筆 / 共{' '}
+                  {filterData.length} 筆
+                </p>
+              )}
               <div className="filtericon" onClick={toggleFilters}>
                 <BsFilterSquareFill />
               </div>
@@ -118,26 +123,24 @@ export default function Tagdog(props) {
                         <p className="searchcategory">種類</p>
                         <p className="line" />
                         <div className="d-flex pet-choose">
-                          <TagCat
-                            setUrl={setUrl}
-                            activeIndex={
-                              active?.c === 'tagcat' ? active.v : null
-                            }
-                            onActiveChange={(v) => ActiveChange('tagcat', v)}
-                          />
-                          <TagDog
+                          <Dog
                             setUrl={setUrl}
                             activeIndex={
                               active?.c === 'tagdog' ? active.v : null
                             }
                             onActiveChange={(v) => ActiveChange('tagdog', v)}
                           />
-                          <TagOther
+                          <OtherDog
                             setUrl={setUrl}
                             activeIndex={
                               active?.c === 'tagother' ? active.v : null
                             }
                             onActiveChange={(v) => ActiveChange('tagother', v)}
+                          />
+                          <List
+                            setUrl={setUrl}
+                            activeIndex={active?.c === 'list' ? active.v : null}
+                            onActiveChange={(v) => ActiveChange('list', v)}
                           />
                         </div>
                       </div>
@@ -147,11 +150,6 @@ export default function Tagdog(props) {
                         <p className="searchpro col">類別</p>
                       </div>
                       <div className="row category-detail d-flex flex-column mx-0">
-                        <CategoryCat
-                          setUrl={setUrl}
-                          activeIndex={active?.c === 'cat' ? active.v : null}
-                          onActiveChange={(v) => ActiveChange('cat', v)}
-                        />
                         <CategoryDog
                           setUrl={setUrl}
                           activeIndex={active?.c === 'dog' ? active.v : null}
@@ -199,20 +197,20 @@ export default function Tagdog(props) {
                     <p className="line" />
                     {/* 狗貓標籤預設 false 灰色 點選後true橘色 */}
                     <div className="d-flex pet-choose">
-                      <TagCat
-                        setUrl={setUrl}
-                        activeIndex={active?.c === 'tagcat' ? active.v : null}
-                        onActiveChange={(v) => ActiveChange('tagcat', v)}
-                      />
-                      <TagDog
+                      <Dog
                         setUrl={setUrl}
                         activeIndex={active?.c === 'tagdog' ? active.v : null}
                         onActiveChange={(v) => ActiveChange('tagdog', v)}
                       />
-                      <TagOther
+                      <OtherDog
                         setUrl={setUrl}
                         activeIndex={active?.c === 'tagother' ? active.v : null}
                         onActiveChange={(v) => ActiveChange('tagother', v)}
+                      />
+                      <List
+                        setUrl={setUrl}
+                        activeIndex={active?.c === 'list' ? active.v : null}
+                        onActiveChange={(v) => ActiveChange('list', v)}
                       />
                     </div>
                   </div>
@@ -224,12 +222,6 @@ export default function Tagdog(props) {
                   </div>
                   {/* 類別細節 */}
                   <div className="row category-detail d-flex flex-column mx-0">
-                    {/* 貓貓專區 + 點開會顯示下列細節再次點選會收起 預設false收起 */}
-                    <CategoryCat
-                      setUrl={setUrl}
-                      activeIndex={active?.c === 'cat' ? active.v : null}
-                      onActiveChange={(v) => ActiveChange('cat', v)}
-                    />
                     {/* 狗狗專區 + 點開會顯示下列細節再次點選會收起 預設false收起 */}
                     <CategoryDog
                       setUrl={setUrl}
@@ -256,11 +248,15 @@ export default function Tagdog(props) {
             <div className="row">
               {/* 顯示數量 每頁幾筆 排序 內容 */}
               <div className="row choose-page">
-                <p className="howmaney col mt-3">
-                  顯示第{nowPageFirstItems + 1}-
-                  {Math.min(nowPageLastItems, filterData.length)} 筆 / 共{' '}
-                  {filterData.length} 筆
-                </p>
+                {filterData.length === 0 ? (
+                  <div></div>
+                ) : (
+                  <p className="howmaney col mt-3">
+                    顯示第{nowPageFirstItems + 1}-
+                    {Math.min(nowPageLastItems, filterData.length)} 筆 / 共{' '}
+                    {filterData.length} 筆
+                  </p>
+                )}
                 <div className="col selectpd rwd-none px-0">
                   <PerPageDom
                     itemsperPage={itemsperPage}
@@ -290,12 +286,16 @@ export default function Tagdog(props) {
               {/* 頁籤 */}
               <div className="d-flex justify-content-center align-items-center mb-5 mt-5">
                 <div className="rwd-block">
-                  <PageNav
-                    nowPage={nowPage}
-                    totalPage={totalPage}
-                    next={next}
-                    prev={prev}
-                  />
+                  {filterData.length === 0 ? (
+                    <div></div>
+                  ) : (
+                    <PageNav
+                      nowPage={nowPage}
+                      totalPage={totalPage}
+                      next={next}
+                      prev={prev}
+                    />
+                  )}
                 </div>
               </div>
             </div>
