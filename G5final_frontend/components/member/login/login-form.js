454222159baae23c133ccc9a75b51087d1c2440e
Edward <edward940715@gmail.com>
2024-11-17 11:37:47 +0800

    新增會員登入可指定路由，useAuth新增nextRoute，可自行設定會員登入後要導向的路由(一次性)

--- G5final_frontend/components/member/login/login-form.js
+++ G5final_frontend/components/member/login/login-form.js
@@ -11,6 +11,7 @@ import { PiEyeClosed } from 'react-icons/pi';
 //google login
 import useFirebase from '@/hooks/use-firebase';
 import { googleLogin } from '@/services/member';
+import { se } from 'date-fns/locale';
 
 export default function LoginForm({ Formtype, setFormtype }) {
   const router = useRouter();
@@ -19,7 +20,15 @@ export default function LoginForm({ Formtype, setFormtype }) {
   const handleFieldChange = (e) => {
     setUser({ ...user, [e.target.name]: e.target.value });
   };
-  const { login, logout, auth, setAuth } = useAuth();
+  const { login, logout, auth, setAuth, nextRoute, setNextRoute } = useAuth();
+
+  const runRoute = () => {
+    // 導向下一個路由
+    router.push(nextRoute);
+    // 設定下一個路由回預設的會員資料頁
+    setNextRoute('/member');
+  }
+
   const handleLogin = async () => {
     // 表單驗證 - START
     if (user.email === '') {
@@ -29,7 +38,49 @@ export default function LoginForm({ Formtype, setFormtype }) {
       return toast.error('請輸入密碼');
     }
     // 表單驗證 - END
-    login(user.email, user.password);
+
+    // 傳統登入
+    try {
+      const res = await axiosInstance.post('/member/login', user);
+      // console.log(res);
+      // 回傳資料
+      // res.json({
+      //   status: 'success',
+      //   token: { accessToken },
+      //   memberdata: dbMember,
+      // })
+
+      if (res.data.status === 'success') {
+        // 將登入成功與取回的會員資料設定到全域狀態auth，其他頁面可以直接取用
+        // 使用 ?? 可以 接受false與0的值  null undefined都會被排除
+        setAuth({
+          isAuth: true,
+          memberData: {
+            ...auth.memberData,
+            id: res.data.memberData.ID ?? '',
+            name: res.data.memberData.Name ?? '',
+            email: res.data.memberData.eMail ?? '',
+            nickname: res.data.memberData.Nickname ?? '',
+            avatar: res.data.memberData.Avatar ?? '',
+            google_uid: res.data.memberData.google_uid ?? '',
+            google_avatar: res.data.memberData.google_avatar ?? '',
+            isPetCom: res.data.memberData.isPetCom ?? '',
+          },
+        });
+        // 顯示登入成功訊息
+        toast.success(res.data.message);
+        // 執行路由導向
+        runRoute();
+      } else {
+        toast.error(res.data.message);
+      }
+    } catch (error) {
+      console.error('Error fetching member data:', error);
+      return {}; // 返回空物件，表示錯誤時不會崩潰
+    }
+
+
+
   };
   const { loginGoogle } = useFirebase();
 
@@ -65,8 +116,8 @@ export default function LoginForm({ Formtype, setFormtype }) {
             },
           };
           setAuth(nextAuth);
-          // 導向到會員中心
-          router.push('/member');
+         // 執行路由導向
+         runRoute();
         }
       } catch (e) {
         console.error(e);
@@ -149,7 +200,7 @@ export default function LoginForm({ Formtype, setFormtype }) {
                 className={`${styles['btn-google']}`}
               />
               <span className={`${styles['span-google']}`}>Google帳號登入</span>
-              
+
             </button>
           </div>
           <div className="d-flex justify-content-between">
--- G5final_frontend/hooks/use-auth.js
+++ G5final_frontend/hooks/use-auth.js
@@ -29,54 +29,8 @@ export function AuthProvider({ children }) {
     memberData: initMemberData,
   });
 
-  // 會員登入
-  const login = async (email, password) => {
-    try {
-      const res = await axiosInstance.post('/member/login', {
-        email,
-        password,
-      });
-      // console.log(res);
-      // 回傳資料
-      // res.json({
-      //   status: 'success',
-      //   token: { accessToken },
-      //   memberdata: dbMember,
-      // })
-
-      if (res.data.status === 'success') {
-        // 可以解析jwt token得出id,name (!後續要加上google_uid)
-        // const jwtData = parseJwt(res.data.token.accessToken);
-        // console.log(jwtData);
-
-        // 將登入成功與取回的會員資料設定到全域狀態auth，其他頁面可以直接取用
-        // 使用 ?? 可以 接受false與0的值  null undefined都會被排除
-        setAuth({
-          isAuth: true,
-          memberData: {
-            ...auth.memberData,
-            id: res.data.memberData.ID ?? '',
-            name: res.data.memberData.Name ?? '',
-            email: res.data.memberData.eMail ?? '',
-            nickname: res.data.memberData.Nickname ?? '',
-            avatar: res.data.memberData.Avatar ?? '',
-            google_uid: res.data.memberData.google_uid ?? '',
-            google_avatar: res.data.memberData.google_avatar ?? '',
-            isPetCom: res.data.memberData.isPetCom ?? '',
-          },
-        });
-        // 導向到會員中心
-        router.push('/member');
-        // 顯示登入成功訊息
-        toast.success(res.data.message);
-      } else {
-        toast.error(res.data.message);
-      }
-    } catch (error) {
-      console.error('Error fetching member data:', error);
-      return {}; // 返回空物件，表示錯誤時不會崩潰
-    }
-  };
+  // 希望導向下一個頁面
+  const [nextRoute, setNextRoute] = useState('/member');
 
   // 會員登出
   const logout = async () => {
@@ -183,7 +137,7 @@ export function AuthProvider({ children }) {
 
   return (
     <AuthContext.Provider
-      value={{ auth, setAuth, login, logout, getMember, initMemberData }}
+      value={{ auth, setAuth, logout, getMember, initMemberData, nextRoute, setNextRoute }}
     >
       <Toaster />
       {children}
